9a9175851f92384c8beb6a301a83f29da094b0ae
MetallicSquid <guillame.Macneil@ccoex.com>
2020-06-23 17:41:33 +0100

    commit for data processing

--- data_preparation.py
+++ data_preparation.py
@@ -1,6 +1,7 @@
 # Imports
 import csv
 import pandas as pd
+import json
 
 # Convert dataset to dataframe
 dataframe = pd.read_csv("symptoms_illness_dataset.csv")
@@ -35,8 +36,28 @@ for index in range(len(illness_list[2:])):
         illness_list = illness_list[:index]
         break
 
-# Equalize the length of the lists
+# Make a list of all of the symptoms -- this will be used later
+symptom_list = []
+for element in illness_list:
+    symptoms = element[1:]
+    for symptom in symptoms:
+        symptom_list.append(symptom)
+# Remove duplicates (I don't know how it works, it just does)
+symptoms = list(dict.fromkeys(symptom_list))
 
+# Find the element of greatest length in the array
+greatest = 0
+for element in illness_list:
+    length = len(element)
+    if length > greatest:
+        greatest = length
+
+# Equalize the length of the lists
+for element in illness_list:
+    length = len(element)
+    difference = greatest - length
+    for cycle in range(difference):
+        element.append("None")
 
 # Make a dictionary out of the remaining list
 illness_dict = {}
@@ -44,4 +65,22 @@ for element in illness_list:
     illness_dict[element[0]] = element[1:]
 
 # `illness_dict` compares each disease to their symptoms
-print(illness_dict)
+illness_dataframe = pd.DataFrame(data=illness_dict)
+#print(illness_dataframe)
+
+#########################################
+# At this point, the dataset is ready.  #
+# Now to make the intents.json file.    #
+#########################################
+
+json_data = {}
+
+for symptom in symptoms:
+    json_data[symptom] = {}
+
+# Wrap the data in the "intents" tag
+json_data = {"intents": [json_data]} 
+
+print(json_data)
+with open('intents.json', 'w') as outfile:
+    json.dump(json_data, outfile)
